ae4e61a92322fe63e9b3ecb357eebdd4f8c9c376
efortier <eddy.fortier@umontreal.ca>
2022-02-26 13:21:54 -0500

    corrected paths with os.path.join

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from src import BIDS_formater as formater
 from src import json_sidecar_generator as jsg
 from src import MRI_session_design_generator as ses_design
@@ -9,6 +10,9 @@ ls_fct = ["BIDS format's json sidecars (test level) creation",
           "BIDS format's auditory data exporter",
           "Pure-Tone Audiometry graph generator",
           "Matrix Speech-in-Noise Test graph generator",
+          "Transient-evoked OAE test graph generator",
+          "Distortion product OAE test graph generator",
+          "Distortion product growth function test graph generator",
           "MRI session design files generator"]
 
 # Prompt text generation
@@ -58,12 +62,12 @@ while loop_value:
 
                     if ls_fct[value - 1] == ("BIDS format's json sidecars "
                                              "(test level) creation"):
-                        jsg.create_sidecars("./results")
+                        jsg.create_sidecars(os.path.join(".", "results")
                         print("\n")
 
                     elif ls_fct[value - 1] == ("BIDS format's auditory "
                                                "data exporter"):
-                        formater.master_run("./results")
+                        formater.master_run(os.path.join(".", "results")
                         print("\n")
 
                 # Graph generation functionalities
@@ -71,18 +75,49 @@ while loop_value:
 
                     # PTA graph plotting
                     if ls_fct[value - 1].count("Pure-Tone") == 1:
-                        graph.master_run("./results", "PTA")
+                        graph.master_run(os.path.join(".", "results"), "PTA")
                         print("\n")
 
                     # MTX graph plotting
                     elif ls_fct[value - 1].count("Matrix") == 1:
-                        graph.master_run("./results", "MTX")
+                        graph.master_run(os.path.join(".", "results"), "MTX")
                         print("\n")
+
+                    # TEOAE graph plotting
+                    elif ls_fct[value - 1].count("Transient") == 1:
+                        print("TEOAE graph: This functionality is not "
+                              "supported yet but will be added soon.\n")
+                        # TEOAE graph plotting functionalities
+                        print("\n")
+                    
+                    elif ls_fct[value - 1].count("Distortion"):
+                        
+                        # DPOAE graph plotting
+                        if ls_fct[value - 1] == ("Distortion product OAE "
+                                                 "test graph generator"):
+                            print("DPOAE graph: This functionality is not "
+                                  "supported yet but will be added soon.\n")
+                            # DPOAE graph plotting functionalities
+                            print("\n")
+                        
+                        # DP Growth graph plotting
+                        elif ls_fct[value - 1] == ("Distortion product "
+                                                   "growth function test "
+                                                   "graph generator"):
+                            print("DP growth graph: This functionality is not "
+                                  "supported yet but will be added soon.\n")
+                            # DP growth graph plotting functionalities
+                            print("\n")
+                            
 
                 # MRI sessions design files generation functionalities
                 elif ls_fct[value - 1].count("design files") == 1:
-                    ses_design.master_run("./data")
+                    ses_design.master_run(os.path.join(".", "data")
                     print("\n")
+
+                # Test Dummy
+                elif ls_fct[value - 1].count("Dummy") == 1:
+                    print("This is just a test line:", ls_fct[value - 1], "\n")
 
 ###############################################################################
 
