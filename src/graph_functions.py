20f1bd12792c50a078072951e01624f455d93306
efortier <eddy.fortier@umontreal.ca>
2022-03-15 17:30:08 -0400

    added a function to create the folders to be able to save the graphs

--- src/graph_functions.py
+++ src/graph_functions.py
@@ -1,4 +1,7 @@
+import os
 import pandas as pd
+
+from src import common_functions as common
 
 
 if __name__ == "__main__":
@@ -6,6 +9,43 @@ if __name__ == "__main__":
           "Please use graph_generator.py to call it.")
 
 else:
+    def result_location(result_path, ls_subjects):
+        """
+        This function makes sure that the destination for the graph files
+        exists. If it doesn't, this function creates it.
+        INPUTS:
+        -result_path: path of the results folder [repo_root]/results/
+        -ls_subjects: list of the subjects IDs
+        OUTPUTS:
+        -prints some feedback lines to the user
+        -NO specific return to the script
+        """
+
+        # Results location existence verifications
+        content_result_path = os.listdir(result_path)
+        content_result_path.sort()
+
+        # Verification of the existence of the "graphs" folder
+        # -> destination of the created .html graph files:
+        #    "[repository_root]/results/graphs/"
+        if content_result_path.count("graphs") == 1:
+            print("The results/graphs/ folder is present.\n")
+        else:
+            os.mkdir(os.path.join(result_path, "graphs"))
+            print("The results/graphs/ folder was created.\n")
+
+        subfolder_path = os.path.join(result_path, "graphs")
+        
+        content_subfolder = os.listdir(subfolder_path)
+        content_subfolder.sort()
+
+        # Verification of the existence/creation of each of the
+        # subjects' subfolders
+        for i in ls_subjects:
+            common.create_folder_subjects(i, subfolder_path)
+
+        print("The subjects' subfolders are present in the graphs folder.\n")
+
     def eliminate_row(df, column_to_search, value_to_search):
         """
         INPUTS
