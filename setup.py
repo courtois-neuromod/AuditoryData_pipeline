41e875afd0b5266fd051a37e0b37ca45198cb7d9
efortier <eddy.fortier@umontreal.ca>
2022-03-14 22:02:26 -0400

    added some informations in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import find_packages, setup
 
-setup(name='AuditoryDataPipeline',
+setup(name="AuditoryDataPipeline",
+      version="0.0.0",
+      description="Description is unavailable at the moment.",
+      license="MIT",
+      author="E. Fortier",
+      author_email="eddy.fortier@umontreal.ca",
+      url="https://github.com/eddyfortier/AuditoryData_pipeline",
+      install_requires=["numpy", "pandas", "plotly"],
       packages=find_packages())
