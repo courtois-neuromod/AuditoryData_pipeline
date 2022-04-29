6d48ba00abfdc3878d21cfa67c931f77248fc833
efortier <eddy.fortier@umontreal.ca>
2022-04-29 09:22:29 -0400

    added sklearn as a requirement

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name="AuditoryDataPipeline",
       author="E. Fortier",
       author_email="eddy.fortier@umontreal.ca",
       url="https://github.com/eddyfortier/AuditoryData_pipeline",
-      install_requires=["numpy", "pandas", "plotly", "matplotlib"],
+      install_requires=["numpy", "pandas", "plotly", "matplotlib", "sklearn"],
       packages=find_packages())
