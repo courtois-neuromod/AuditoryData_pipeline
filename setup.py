0a899f207bd7e30ae6ead46279692c5dace79d4a
efortier <eddy.fortier@umontreal.ca>
2023-05-31 13:14:01 -0400

    license change (MIT -> CC BY-SA 4.0) in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(name="AuditoryDataPipeline",
       version="1.0.0",
       description="Description is unavailable at the moment.",
-      license="MIT",
+      license="Creative Commons Attribution Share Alike 4.0 International",
       author="E. Fortier",
       author_email="eddy.fortier@umontreal.ca",
       url="https://github.com/eddyfortier/AuditoryData_pipeline",
